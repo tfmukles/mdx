0368787886ec2d9b6897cadb8b3560ad0dad7940
Mukles <mukleshossen1763@gmail.com>
2025-04-29 11:24:57 +0600

    Rename package to "sitepins-mdx-2" and update Vite configuration for consistency and formatting improvements.

--- vite.config.ts
+++ vite.config.ts
@@ -1,60 +1,60 @@
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'MyMdxPackage',
+      entry: "src/index.ts",
+      name: "sitepins-mdx-2",
       fileName: (format) => {
-        if (format === 'es') return 'index.mjs'
-        if (format === 'umd') return 'index.browser.mjs'
-        return 'index.js'
+        if (format === "es") return "index.mjs";
+        if (format === "umd") return "index.browser.mjs";
+        return "index.js";
       },
-      formats: ['es', 'cjs', 'umd']
+      formats: ["es", "cjs", "umd"],
     },
     rollupOptions: {
       external: [
-        'acorn',
-        'ccount',
-        'estree-util-is-identifier-name',
-        'lodash.flatten',
-        'mdast-util-compact',
-        'mdast-util-directive',
-        'mdast-util-from-markdown',
-        'mdast-util-gfm',
-        'mdast-util-mdx-jsx',
-        'mdast-util-to-markdown',
-        'micromark-extension-gfm',
-        'micromark-factory-mdx-expression',
-        'micromark-factory-space',
-        'micromark-factory-whitespace',
-        'micromark-util-character',
-        'micromark-util-symbol',
-        'micromark-util-types',
-        'parse-entities',
-        'prettier',
-        'remark',
-        'remark-gfm',
-        'remark-mdx',
-        'stringify-entities',
-        'unist-util-source',
-        'unist-util-stringify-position',
-        'unist-util-visit',
-        'uvu',
-        'vfile-message'
-      ]
-    }
+        "acorn",
+        "ccount",
+        "estree-util-is-identifier-name",
+        "lodash.flatten",
+        "mdast-util-compact",
+        "mdast-util-directive",
+        "mdast-util-from-markdown",
+        "mdast-util-gfm",
+        "mdast-util-mdx-jsx",
+        "mdast-util-to-markdown",
+        "micromark-extension-gfm",
+        "micromark-factory-mdx-expression",
+        "micromark-factory-space",
+        "micromark-factory-whitespace",
+        "micromark-util-character",
+        "micromark-util-symbol",
+        "micromark-util-types",
+        "parse-entities",
+        "prettier",
+        "remark",
+        "remark-gfm",
+        "remark-mdx",
+        "stringify-entities",
+        "unist-util-source",
+        "unist-util-stringify-position",
+        "unist-util-visit",
+        "uvu",
+        "vfile-message",
+      ],
+    },
   },
   plugins: [
     dts({
       insertTypesEntry: true,
     }),
   ],
-  assetsInclude: ['**/*.md'],
+  assetsInclude: ["**/*.md"],
   resolve: {
     alias: {
-      '^.*\\.md\\?raw$': '$&'
-    }
-  }
-}) 
+      "^.*\\.md\\?raw$": "$&",
+    },
+  },
+});
